1f33f5b3f42845321d89bf36d867d94dd700abe2
DevTechJr <devtechjr@gmail.com>
2024-07-26 03:13:46 -0400

    worked till 3am with bhavya and sajan on tasks

--- docs/__init__.py
+++ docs/__init__.py
@@ -30,7 +30,7 @@ loginmanager.login_view = "login_page" #When unauthorized user tries clicking th
 
 #Obtain routes from routes.py in the docs module
 from docs import routes
-from docs.models import User, Purchases
+from docs.models import User, Purchases, Options, Cart
 
 class AdminModelView(ModelView):
   can_edit = False
@@ -46,13 +46,12 @@ class AdminModelView(ModelView):
       return redirect(url_for('login_page'))
    
 class PurchaseModelView(ModelView):
-    """
     can_delete = True
     column_searchable_list = ['type', 'title']
-    # column_exclude_list = ['id']
+    column_exclude_list = ['id']
     column_filters = ['purchased_date', 'type']
-    form_excluded_columns = ['title', 'type', 'price', 'purchased_date', 'owner']
-    """
+    form_excluded_columns = ['title', 'type', 'price', 'purchased_date', 'owner', 'yearly_price', 'monthly_price']
+    
     
     def is_accessible(self):
       if current_user.is_authenticated: 
@@ -61,6 +60,40 @@ class PurchaseModelView(ModelView):
     def inaccessible_callback(self, name, **kwargs):
     # redirect to login page if user doesn't have access
       return redirect(url_for('login_page'))
+
+class OptionsModelView(ModelView):
+  can_edit = True
+  column_searchable_list = ['type', 'title']
+  column_filters = ['type']
+  column_exclude_list = ['id', 'colour']
+  form_excluded_columns = ['colour', 'type']
+
+  def is_accessible(self):
+    if current_user.is_authenticated: 
+        return current_user.isAdmin
+    
+  def inaccessible_callback(self, name, **kwargs):
+  # redirect to login page if user doesn't have access
+    return redirect(url_for('login_page'))
+  
+class CartModelView(ModelView):
+  """
+  can_delete = True
+  column_searchable_list = ['type', 'title']
+  # column_exclude_list = ['id']
+  column_filters = ['purchased_date', 'type']
+  form_excluded_columns = ['title', 'type', 'price', 'purchased_date', 'owner']
+  """
+  
+  def is_accessible(self):
+    if current_user.is_authenticated: 
+        return current_user.isAdmin
+    
+  def inaccessible_callback(self, name, **kwargs):
+  # redirect to login page if user doesn't have access
+    return redirect(url_for('login_page'))
     
 admin.add_view(AdminModelView(User, db.session ))
 admin.add_view(PurchaseModelView(Purchases, db.session ))
+admin.add_view(OptionsModelView(Options, db.session ))
+admin.add_view(CartModelView(Cart, db.session ))
--- docs/routes.py
+++ docs/routes.py
@@ -1,12 +1,16 @@
 from docs import app, db, render_template, redirect, url_for, flash
 from docs import login_user, logout_user, current_user, login_required
 from docs.forms import RegisterForm, LoginForm
-from docs.models import User
+from docs.models import User,Cart
+from flask import request
+from docs.models import Options, Purchases
 
 
 @app.route("/")
 def index_page():
   return render_template("index.html")
+
+
 
 @app.route("/register", methods = ["POST", "GET"])
 def register_page():
@@ -63,6 +67,107 @@ def logout_page():
 @app.route("/dashboard", methods = ["POST", "GET"])
 def dashboard_page():
   
-  return render_template("dashboard.html", user = current_user)
+  approved = Purchases.query.filter_by(owner = current_user.id, status = "APPROVED").all()
+  pending = Purchases.query.filter_by(owner = current_user.id, status = "PENDING").all()
+  declined = Purchases.query.filter_by(owner = current_user.id, status = "DECLINED").all()
 
+  return render_template("dashboard.html", user = current_user, approved = approved, pending = pending, declined = declined)
 
+@login_required
+@app.route("/products", methods = ["POST", "GET"])
+def products_page():
+  if request.method == "POST":
+    return render_template("products.html", current_user=current_user)
+
+  elif request.method == "GET":
+    options = Options.query.all()
+    for option in options:
+      print(option.title)
+
+  cart_items = Cart.query.filter_by(owner = current_user.id).all()
+  for cart_item in cart_items:
+    print("CART ITEM: ", cart_item.title)
+
+  options = Options.query.all()
+  cart = Cart.query.all()
+  return render_template("products.html", current_user=current_user, options=options)
+
+@login_required
+@app.route("/cart", methods = ["POST", "GET"])
+def add_to_cart():
+  product_id = request.args.get("product_id")
+  print("PRODUCT ID: ", product_id)
+  options = Options.query.all()
+  option = Options.query.filter_by(id = product_id).first()
+  # extract all the values from the option object
+  title = option.title
+  type = option.type
+  yearly_price = option.yearly_price
+  month_price = option.month_price
+  mini_description = option.mini_description
+  description = option.description
+  colour = option.colour
+  tier = option.tier
+  
+  new_cart_item = Cart(
+  title=title, type=type, yearly_price=yearly_price,
+  month_price=month_price, mini_description=mini_description,
+  description=description, colour=colour, tier=tier, owner=current_user.id )
+ 
+  with app.app_context():
+    db.session.add(new_cart_item)
+    db.session.commit()
+    flash(f"Added {title} to your cart!", category="success")
+
+  cart = Cart.query.all()
+  
+  return render_template("products.html", user=current_user, options = options, cart=cart)
+
+@login_required
+@app.route("/checkout", methods = ["POST", "GET"])
+def checkout_page():
+
+  cart_items = Cart.query.filter_by(owner = current_user.id)
+  if request.method == "POST":
+      for item in cart_items:
+        id = item.id
+        title = item.title
+        type = item.type
+        yearly_price = item.yearly_price
+        month_price = item.month_price
+        mini_description = item.mini_description
+        description = item.description
+        colour = item.colour
+        tier = item.tier
+
+        # Add to the purchases table
+        new_purchase = Purchases(
+          title=title, type=type, yearly_price=yearly_price,
+          month_price=month_price, owner=current_user.id, status="PENDING" )
+        # with app.app_context():
+        db.session.add(new_purchase)
+        db.session.commit()
+
+        # Delete from the cart table
+        cart_item = Cart.query.filter_by(id=id).first()
+        db.session.delete(cart_item)
+        db.session.commit()
+        flash(f"Added {title} to your purchases!", category="success")
+      
+  
+  return render_template("checkout.html", cart_items = cart_items)
+
+@login_required
+@app.route("/delete", methods=["POST", "GET"])
+def delete_cart_item():
+    cart_item_id = request.args.get("cart_item_id")
+    cart_item = Cart.query.filter_by(id=cart_item_id).first()
+
+    if cart_item:
+        db.session.delete(cart_item)
+        db.session.commit()
+        flash(f"Deleted {cart_item.title} from your cart!", category="success")
+    else:
+        flash("Item not found in cart.", category="error")
+
+    return redirect(url_for("checkout_page"))
